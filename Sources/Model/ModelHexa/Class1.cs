27da802181430f58a21d0d87f9728c119d8d0c91
Houari BEY <hobey>
2025-04-02 19:12:04 +0200

    F3 ajout projet model

---
+++ Sources/Model/ModelHexa/Class1.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Maui.Graphics.Platform;
+
+namespace ModelHexa
+{
+    public class Pawn
+    {
+        private
+            bool isFirstMove;
+    }
+}
---
+++ Sources/Model/ModelHexa/Platforms/Tizen/PlatformClass1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ModelHexa
+{
+    // All the code in this file is only included on Tizen.
+    public class PlatformClass1
+    {
+    }
+}
