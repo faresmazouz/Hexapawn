bd89f7b22b2fe09183652b37c15a6719306d6c84
Fares MAZOUZ <famazouz>
2025-04-09 18:34:30 +0200

    ok

---
+++ Sources/Model/ModelHexa/Cell.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Graphics.Platform;
+
+namespace ModelHexa
+{
+    class Cell
+    {
+        public int X; { get; set; }
+        public int Y; { get; set; }
+        private Pawn? pawn;
+        public Cell(int x, int y)
+        {
+            X = x;
+            Y = y;
+            pawn = null;
+        }
+        public bool Empty()
+        {
+            return pawn == null;
+        }
+        public void setPawn(Pawn pawn)
+        {
+            pawn = p;
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
