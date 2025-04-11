6c727c20d95e65554bb42520e59a8e36f50fc3c8
Thomas ALVES <thalves>
2025-04-11 09:51:33 +0200

    ajout de classe règle

---
+++ Sources/Model/ModelHexa/Rules.cs
@@ -0,0 +1,18 @@
+﻿
+using GameController;
+
+namespace ModelHexa
+{
+    public class Rules
+    {
+        public bool isMoveValid(Board b, Move move, TeamColor t, Cell c)
+        {
+            if (c.pawn==null|| c.pawn.Color!=t) return false;
+            if (move == Move.cantMove) return false;
+            if ( move==Move.eatLeft)
+            {
+                
+            }
+        }
+    }
+}
