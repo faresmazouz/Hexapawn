807a1cb66be1a7fd0781cc67faf3aa8dcf833521
Thomas ALVES <thalves>
2025-04-11 09:26:07 +0200

    résolution de conflits

--- Sources/Model/ModelHexa/Cell.cs
+++ Sources/Model/ModelHexa/Cell.cs
@@ -1,35 +1,24 @@
-<<<<<<< HEAD
-﻿
 namespace ModelHexa
-=======
-﻿namespace ModelHexa
->>>>>>> 0a54c4631e1d3e44aa10dce24f4c965296101798
 {
     public class Cell
     {
         public int X { get; set; }
         public int Y { get; set; }
         private Pawn? pawn;
-<<<<<<< HEAD
-=======
+
+
         public Cell(int x, int y, Pawn p)
         {
             X = x;
             Y = y;
             pawn = p;
         }
-
->>>>>>> 0a54c4631e1d3e44aa10dce24f4c965296101798
         public Cell(int x, int y)
         {
             X = x;
             Y = y;
             pawn = null;
         }
-<<<<<<< HEAD
-    }
-=======
-
         public bool Empty()
         {
             return pawn == null;
@@ -40,8 +29,6 @@ namespace ModelHexa
         }
 
     }
-
->>>>>>> 0a54c4631e1d3e44aa10dce24f4c965296101798
 }
 
 
--- Sources/Model/ModelHexa/Pawn.cs
+++ Sources/Model/ModelHexa/Pawn.cs
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-﻿using Microsoft.Maui.Graphics.Platform;
-
 namespace ModelHexa
 {
     public struct Pawn
@@ -21,28 +18,4 @@ namespace ModelHexa
         Player1,
         Player2,
     }
-=======
-﻿using Microsoft.Maui.Graphics.Platform;
-
-namespace ModelHexa
-{
-    public struct Pawn
-    {
-        public TeamColor Color { get; set; }
-        public Pawn()
-        {
-            Color = TeamColor.Unknown;
-        }
-        public Pawn(TeamColor T)
-        {
-            Color = T;
-        }
-    }
-    public enum TeamColor
-    {
-        Unknown,
-        Player1,
-        Player2,
-    }
->>>>>>> 0a54c4631e1d3e44aa10dce24f4c965296101798
 }
