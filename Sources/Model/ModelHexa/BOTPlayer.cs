1c9552920abe2dfaf773486f9f382e1b06e81946
Fares MAZOUZ <famazouz>
2025-05-23 08:30:56 +0200

    tentative de régler un security hotspot

--- Sources/Model/ModelHexa/BOTPlayer.cs
+++ Sources/Model/ModelHexa/BOTPlayer.cs
@@ -8,22 +8,19 @@ namespace ModelHexa
 {
     public class BOTPlayer: Player
     {
+        private static readonly Random rdm = new Random();
         public BOTPlayer(TeamColor teamColor):base("Robot",teamColor) { }
         public override Move ChooseMove(List<Move> l, Cell c, ref bool choixFait)
         {
-            Random rdm = new Random();              //déclare un objet random
+                          //déclare un objet random
             Move m = l[rdm.Next(l.Count)];          //prend un mouvement dans la liste d'indice aléatoire de 0 à l'indice max de la liste
             choixFait = true;                       //On met le choix à true pour que ça ne se répète qu'une fois
             return m;
         }
         public override Cell ChoosePawn(Dictionary<Cell, List<Move>> dict)
         {
-            List<Cell> listOfPawn = []; 
-            Random rdm = new Random();  //déclare un objet random
-            foreach (Cell c in dict.Keys)
-            {
-                listOfPawn.Add(c);
-            }
+            List<Cell> listOfPawn = new List<Cell>(dict.Keys); 
+
             return listOfPawn[rdm.Next(listOfPawn.Count)];  //Renvoie un élément aléatoire de la liste des pions
 
             
