e75b3bf072d800baf9b45acec056da805a9dacff
Fares MAZOUZ <famazouz>
2025-05-23 08:54:25 +0200

    merge master security

--- Sources/Model/ModelHexa/BOTPlayer.cs
+++ Sources/Model/ModelHexa/BOTPlayer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,27 +7,24 @@ using System.Threading.Tasks;
 
 namespace ModelHexa
 {
-    public class BOTPlayer: Player
+    public class BOTPlayer : Player
     {
-        public BOTPlayer(TeamColor teamColor):base("Robot",teamColor) { }
+        private static readonly Random rdm = new Random();
+        public BOTPlayer(TeamColor teamColor) : base("Robot", teamColor) { }
         public override Move ChooseMove(List<Move> l, Cell c, ref bool choixFait)
         {
-            Random rdm = new Random();              //déclare un objet random
+
             Move m = l[rdm.Next(l.Count)];          //prend un mouvement dans la liste d'indice aléatoire de 0 à l'indice max de la liste
             choixFait = true;                       //On met le choix à true pour que ça ne se répète qu'une fois
             return m;
         }
         public override Cell ChoosePawn(Dictionary<Cell, List<Move>> dict)
         {
-            List<Cell> listOfPawn = []; 
-            Random rdm = new Random();  //déclare un objet random
-            foreach (Cell c in dict.Keys)
-            {
-                listOfPawn.Add(c);
-            }
+            List<Cell> listOfPawn = new List<Cell>(dict.Keys);
+
             return listOfPawn[rdm.Next(listOfPawn.Count)];  //Renvoie un élément aléatoire de la liste des pions
 
-            
+
         }
     }
 }
