ad8c6cea69c8d2479ab8acc53ba47c6abb4b90fb
Houari BEY <houari.bey@etu.uca.fr>
2025-05-27 11:20:50 +0200

    Rétablir : dc77e1f4ff294e724bf8bc6429746443c78d1ee6
    je regle les betises de fares

--- Sources/ConsoleApp/Program.cs
+++ Sources/ConsoleApp/Program.cs
@@ -6,17 +6,20 @@ using ConsoleApp;
 using ModelHexa;
 
 
-bool launchGame(ref Dictionary<string, int> dictScores)
+Dictionary<string, int> dictScores = new Dictionary<string, int>();
+while (LaunchGame(ref dictScores)) ;
+
+bool LaunchGame(ref Dictionary<string, int> dictScores)
 {
     AfficherActions();
     int choix = DemanderChoixAction();
     switch (choix)
     {
         case 1:
-            while (createPart(ref dictScores)) ;
+            LancerPartie(ref dictScores);
             break;
         case 2:
-            while (checkScores(dictScores)) ;
+            AfficherScores(dictScores);
             break;
     }
     return !DemanderQuitter();
@@ -24,13 +27,20 @@ bool launchGame(ref Dictionary<string, int> dictScores)
 
 void AfficherActions()
 {
+
+
+    string stop;
+    int i = 1, choix;
+    bool saisieValide = false;
+
     Console.WriteLine("Liste des différentes actions:");
     int i = 1;
     foreach (ActionDebut act in Enum.GetValues(typeof(ActionDebut)))
     {
-        Console.WriteLine($"{i}. {chois(act)}");
+        Console.WriteLine($"{i}. {GetActionLabel(act)}");
         i++;
     }
+
 }
 
 int DemanderChoixAction()
@@ -46,32 +56,58 @@ int DemanderChoixAction()
 
 bool DemanderQuitter()
 {
+
+    do
+    {
+    Console.Write("Entrez le numéro de l'action à réaliser: ");
+    string input = Console.ReadLine();
+
+    if (int.TryParse(input, out choix) && choix >= 1 && choix < i)
+    {
+        saisieValide = true;
+    }
+    else
+    {
+        Console.WriteLine("Entrée invalide. Veuillez entrer un nombre entier valide entre 1 et " + (i - 1));
+    }
+    } while (!saisieValide);
+    if (choix == 1)
+    {
+        while (createPart(ref dictScores)) ;
+    }
+    else if (choix == 2)
+    {
+        while (checkScores(dictScores)) ;
+    }
+
     Console.WriteLine("Quitter l'application oui(y) ou non (n'importe quoi d'autre) ?");
     return Console.ReadLine() == "y";
 }
 
-string chois(ActionDebut act)
+string GetActionLabel(ActionDebut act) => act switch
 {
-    return act switch
-    {
-        ActionDebut.LancerPartie => "Lancer une partie ",
-        ActionDebut.CheckScores => "Voir les scores des joueurs",
-        _ => "Action inconnue"
-    };
-}
+    ActionDebut.LancerPartie => "Lancer une partie ",
+    ActionDebut.CheckScores => "Voir les scores des joueurs",
+    _ => "Action inconnue"
+};
 
-bool checkScores(Dictionary<string, int> dictScores)
+void AfficherScores(Dictionary<string, int> dictScores)
 {
     var classement = CalculerClassement(dictScores);
     Console.WriteLine("Pour voir tous les joueurs, appuyez sur \"entrer\", sinon entrez le nom du joueur");
     string choix = Console.ReadLine();
     if (string.IsNullOrEmpty(choix))
-        affichClassement(dictScores, classement);
-    else if (dictScores.ContainsKey(choix))
-        Console.WriteLine($"{classement[choix]}. {choix} : {dictScores[choix]} victoires");
+        AfficherClassement(dictScores, classement);
+    else
+        AfficherScoreJoueur(dictScores, classement, choix);
+}
+
+void AfficherScoreJoueur(Dictionary<string, int> dictScores, Dictionary<string, int> classement, string nom)
+{
+    if (dictScores.ContainsKey(nom))
+        Console.WriteLine($"{classement[nom]}. {nom} : {dictScores[nom]} victoires");
     else
         Console.WriteLine("Aucun joueur n'a ce nom là");
-    return false;
 }
 
 Dictionary<string, int> CalculerClassement(Dictionary<string, int> dictScores)
@@ -79,21 +115,23 @@ Dictionary<string, int> CalculerClassement(Dictionary<string, int> dictScores)
     var classement = new Dictionary<string, int>();
     int i = 1;
     foreach (var elt in dictScores.OrderByDescending(kvp => kvp.Value))
-    {
         classement[elt.Key] = i++;
-    }
     return classement;
 }
 
-void affichClassement(Dictionary<string, int> dictScores, Dictionary<string, int> dictPos)
+void AfficherClassement(Dictionary<string, int> dictScores, Dictionary<string, int> dictPos)
 {
     foreach (var elt in dictScores.OrderByDescending(kvp => kvp.Value))
-    {
         Console.WriteLine($"{dictPos[elt.Key]}. {elt.Key} : {elt.Value} victoires");
-    }
 }
 
-bool createPart(ref Dictionary<string, int> dictScores)
+
+void LancerPartie(ref Dictionary<string, int> dictScores)
+{
+    while (CreerEtJouerPartie(ref dictScores)) ;
+}
+
+bool CreerEtJouerPartie(ref Dictionary<string, int> dictScores)
 {
     int nbCases = DemanderNbCases();
     Board b = new Board(nbCases);
@@ -103,11 +141,80 @@ bool createPart(ref Dictionary<string, int> dictScores)
     BrancherEvenements(p2);
     if (!DemanderConfirmation())
         return true;
-    b.Affiche();
+    b.affiche();
     TeamColor win = TeamColor.Unknown;
     Rules r = new Rules();
     p1.PlayTurn(r.allMoves(b, p1.teamColor), b, r, p2, ref win);
     Player theWinner = (win == TeamColor.Player1) ? p1 : p2;
+
+
+
+bool createPart(ref Dictionary<string, int> dictScores)
+{
+    Player theWinner;
+    bool bot;
+    Rules r=new Rules();
+    int nbCases;
+    bool saisieValide = false;
+    string rep;
+    string nomJoueur="";
+    TeamColor win=TeamColor.Unknown;
+    do
+    {
+        Console.Write("De combien de cases de longueur voulez-vous que le plateau soit? Entrez un chiffre: ");
+        string input = Console.ReadLine();
+        if (int.TryParse(input, out nbCases) && nbCases >= 3)
+        {
+            saisieValide = true;
+        }
+        else
+        {
+            Console.WriteLine("Entrée invalide. Veuillez entrer un nombre entier valide supérieur à 3");
+        }
+    } while (!saisieValide);
+    Board b= new Board(nbCases);
+    Console.WriteLine("Voulez vous que le 1er joueur soit un BOT?(y/n'importe quoi d'autre)");
+    bot = Console.ReadLine()=="y";
+    if (!bot)
+    {
+        Console.WriteLine("Entrez le nom du 1er joueur");
+        nomJoueur=Console.ReadLine();
+        while (nomJoueur == "" || nomJoueur == "Robot")
+        {
+            Console.WriteLine("Erreur, votre nom ne doit pas être vide et vous ne pouvez pas vous appeler Robot, entrez un nom correct");
+            nomJoueur = Console.ReadLine();
+        }
+    }
+    Player p1 = bot ? new BOTPlayer(TeamColor.Player1) : new HumanPlayer(nomJoueur, TeamColor.Player1);
+    dictScores.TryAdd(p1.Name, 0);
+    Console.WriteLine("Voulez vous que le 2ème joueur soit un BOT?(y/n'importe quoi d'autre)");
+    bot = Console.ReadLine() == "y";
+    if (!bot)
+    {
+        Console.WriteLine("Entrez le nom du 2ème joueur");
+        nomJoueur = Console.ReadLine();
+        while (nomJoueur == "" || nomJoueur == "Robot"||nomJoueur==p1.Name)
+        {
+            Console.WriteLine("Erreur, votre nom ne doit pas être vide et vous ne pouvez pas vous appeler Robot et vous ne pouvez pas avoir le même nom que le 1er joueur, entrez un nom correct");
+            nomJoueur = Console.ReadLine();
+        }
+    }
+    Player p2 = bot ? new BOTPlayer(TeamColor.Player2) : new HumanPlayer(nomJoueur, TeamColor.Player2);
+    dictScores.TryAdd(p2.Name, 0);
+    p1.BoardChanged += OnBoardChanged;//On branche l'objet à l'évènement
+    p2.BoardChanged += OnBoardChanged;//pareil
+    p1.UserChoose += OnUserChoose;
+    p2.UserChoose += OnUserChoose;
+    p1.UserHaveToChoose += OnUserHaveToChoose;
+    p2.UserHaveToChoose += OnUserHaveToChoose;
+    Console.WriteLine("Êtes-vous sûr des informations?(y/n\'importe quoi d\'autre)");
+    rep = Console.ReadLine();
+    if (rep != "y") return true;
+    b.Affiche();
+    p1.PlayTurn(r.allMoves(b,p1.teamColor),b,r,p2, ref win);
+    if (win == TeamColor.Player1) theWinner= p1;
+    else theWinner=p2;
+
     theWinner.victoires += 1;
     dictScores[theWinner.Name] += 1;
     Console.WriteLine($"Félicitation, {theWinner.Name} ({dictScores[theWinner.Name]} victoires) a gagné!");
@@ -169,9 +276,6 @@ void OnBoardChanged(object? sender, BoardChangedEventArgs e)
 {
     e.BoardChanged.Affiche();
 }
-
-Dictionary<string, int> dictScores = new Dictionary<string, int>();
-while (launchGame(ref dictScores)) ;
 
 void OnUserHaveToChoose(object? sender, UserHaveToChooseEventArgs e)
 {
