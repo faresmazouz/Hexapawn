ecd72f3cb150e35070e2e366764dec494ee4611d
Fares MAZOUZ <famazouz>
2025-05-28 19:32:19 +0200

    ajout de test databinding

---
+++ Sources/Model/ModelHexa/Manager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ModelHexa;
+namespace Model
+{
+    public class Manager
+    {
+        public Score Victoires { get; private set; }
+            = new Score(987654321);
+
+        public Score Defaites { get; private set; }
+            = new Score(9656565);
+
+        public Score VictoiresVsBot { get; private set; }
+            = new Score(566546);
+
+        public Score DefaitesVsBot { get; private set; }
+            = new Score(54646496);
+    }
+}
