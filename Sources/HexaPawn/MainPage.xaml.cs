6a319ab6d932c137bab6b1808c92340376e17195
fares.mazouz <fares.mazouz@etu-uca.fr>
2025-06-01 10:53:29 +0200

    regler problemes vues

--- Sources/HexaPawn/MainPage.xaml.cs
+++ Sources/HexaPawn/MainPage.xaml.cs
@@ -1,4 +1,4 @@
-namespace solution;
+namespace Hexapawn;
 
 public partial class MainPage : ContentPage
 {
--- Sources/Model/ModelHexa/Manager.cs
+++ Sources/Model/ModelHexa/Manager.cs
@@ -4,20 +4,20 @@ namespace ModelHexa;
 public class Manager
 {
     public Score Victoires { get; private set; }
-        = new Score();
+        = new Score(89);
 
     public Score Defaites { get; private set; }
-        = new Score();
+        = new Score(42);
 
     public Score VictoiresVsBot { get; private set; }
-        = new Score();
+        = new Score(42);
 
     public Score DefaitesVsBot { get; private set; }
-        = new Score();
+        = new Score(42);
 
     public Score PartiesTotales { get; private set; }
-        = new Score();
+        = new Score(42);
 
     public Score PartiesVsBot { get; private set; }
-        = new Score();
+        = new Score(42);
 }
